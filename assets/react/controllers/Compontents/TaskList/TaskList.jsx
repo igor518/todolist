2bb935c83062f23356a6508a84de0d93ef415853
Ukrainian_panda <94929618+Ukrainiane-panda@users.noreply.github.com>
2025-05-09 21:19:08 +0300

    fixed: prevent multiple renders of DisplayUser during loading and data fetching

--- assets/react/controllers/Compontents/TaskList/TaskList.jsx
+++ assets/react/controllers/Compontents/TaskList/TaskList.jsx
@@ -1,5 +1,10 @@
+import { TailChase } from "ldrs/react";
+
 const TaskList = ({taskLists, loading, error}) => {
-    if (loading) return <p>Loading...</p>;
+    if (loading) return (
+        <TailChase size="40" speed="1.75" color="#03A9F4" />
+    );
+
     if (error) return <p>Error loading tasks.</p>;
     return (
         <>
--- assets/react/controllers/Dashboard.jsx
+++ assets/react/controllers/Dashboard.jsx
@@ -1,5 +1,4 @@
-import React from 'react';
-import { useState } from 'react';
+import React, { useEffect, useState, useCallback, useMemo } from 'react';
 import Button from "./Compontents/Button/Button";
 import TaskListContainer from "./Compontents/TaskListContainer/TaskListContainer";
 import TaskListFormContainer from "./Compontents/TaskListFormContainer/TaskListFormContainer";
@@ -14,32 +13,42 @@ import User from './Compontents/User/User'
  * @constructor
  */
 export default function ({id, host}) {
-
-    const client = new ApolloClient({
-        uri: "https://"+ host + "/api/graphql",
-        cache: new InMemoryCache(),
-    });
+    const client = useMemo(() => {
+        return new ApolloClient({
+          uri: `https://${host}/api/graphql`,
+          cache: new InMemoryCache(),
+        });
+      }, [host]);
 
     const [content, setContent] = useState('');
 
-    const addNewTaskList = () => {
+    const addNewTaskList = useCallback(() => {
+        console.log("Add new task clicked");
         setContent('task_list');
-    }
+    }, []);
+
+    useEffect(() => {
+        console.log("Form mounted");
+    }, []);
+
     return (
         <>
             <ApolloProvider client={client}>
                 <div className='tw:flex'>
                     <aside className="tw:fixed tw:flex tw:flex-col tw:top-[96px] tw:left-0 tw:h-[calc(100vh-96px)] tw:w-[360px] tw:bg-gradient-to-b tw:from-[#E3F2FD] tw:to-[#F8FAFC] tw:text-text-main tw:p-4 tw:shadow-lg">
+                        <>
+                            <TaskListContainer userId={id} />
+                            <Button onClick={addNewTaskList}>Create a new Task List</Button>
+                            {
+                                content === 'task_list' && <TaskListFormContainer userId={id} />
+                            }
+                        </>
                         <div className="tw:mt-auto">
                             <User userId={id} />
                         </div>
                     </aside> 
                     <main className="tw:ml-[360px] tw:p-4">
-                        <TaskListContainer userId={id} />
-                        <Button onClick={addNewTaskList}>Create a new Task List</Button>
-                        {
-                            content === 'task_list' && <TaskListFormContainer userId={id} />
-                        }
+                        
                     </main>
 
                 </div>
